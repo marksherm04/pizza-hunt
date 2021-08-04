195a7ee1a4fbec982eddd633c0c4f6f3f869d40c
Mark S <marksherm04@gmail.com>
2021-08-04 13:47:18 -0400

    added validators for user name, comments, and replies

--- models/Comment.js
+++ models/Comment.js
@@ -9,10 +9,14 @@ const ReplySchema = new Schema(
 			default: () => new Types.ObjectId()
 		},
 		replyBody: {
-			type: String
+			type: String,
+			required: true,
+			trim: true
 		},
 		writtenBy: {
-			type: String
+			type: String,
+			required: true,
+			trim: true
 		},
 		createdAt: {
 			type: Date,
@@ -22,7 +26,6 @@ const ReplySchema = new Schema(
 	},
 	{
 		toJSON: {
-			virtuals: true,
 			getters: true
 		}
 	}
@@ -30,10 +33,14 @@ const ReplySchema = new Schema(
 
 const CommentSchema = new Schema({
 	writtenBy: {
-		type: String
+		type: String,
+		required: "You must provide a user name!",
+		trim: true
 	},
 	commentBody: {
-		type: String
+		type: String,
+		required: "You must provide a comment!",
+		trim: true
 	},
 	createdAt: {
 		type: Date,
